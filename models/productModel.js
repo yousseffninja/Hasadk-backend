54943a2d242f266de005d31ed84deadfb1528a76
backm <yousseffmohamedd22@gmail.com>
2023-04-26 20:47:21 +0200

    Changing in some permissions

--- models/productModel.js
+++ models/productModel.js
@@ -40,7 +40,7 @@ const productSchema = new mongoose.Schema({
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5,
+        default: 0,
         min: [1, 'Rating must be above 1.0'],
         max: [5, 'Rating must be less 5.0'],
         set: (val) => Math.round(val * 10) / 10,
