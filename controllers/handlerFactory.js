6d10249d5af4decba25605527905ab9a1903e8e7
backm <yousseffmohamedd22@gmail.com>
2023-05-15 22:24:59 +0300

    and photo and review

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -3,19 +3,17 @@ const AppError = require('../utils/appError');
 const APIFeatures = require('../utils/apiFeatures');
 const User = require('../models/userModel');
 
-exports.deleteOne = (Model) =>
+exports.deleteOne = Model =>
     catchAsync(async (req, res, next) => {
         const doc = await Model.findByIdAndDelete(req.params.id);
 
         if (!doc) {
-            return next(new AppError('No document found with that ID. 404'));
+            return next(new AppError('No document found with that ID', 404));
         }
 
         res.status(204).json({
             status: 'success',
-            data: {
-                doc,
-            },
+            data: null
         });
     });
 
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -3,6 +3,7 @@ const catchAsync = require('../utils/catchAsync');
 const APIFeatures = require('../utils/apiFeatures');
 const factory = require('./handlerFactory');
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 
 exports.setProductUserIds = (req, res, next) => {
     if (!req.body.product) req.body.product = req.params.product;
@@ -44,4 +45,24 @@ exports.getAllReviews = catchAsync(async (req, res, next) =>{
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
-exports.deleteReview = factory.deleteOne(Review);
+exports.deleteReview = catchAsync(async (req, res, next) => {
+    const review = await Review.findById(req.params.id)
+
+    let data;
+
+    if (review.user.id === req.user.id){
+        data = await Review.findByIdAndDelete(req.params.id)
+    }
+    else {
+        return next(new AppError('This Review did not belont to you!', 403));
+    }
+
+    if (!data) {
+        return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(201).json({
+        status: 'success',
+        data: null
+    });
+})
